9dc30fe1d501a36bcd734d4316c40c0cab5ac8a7
KirillovPavel <useless-junk@yandex.ru>
2020-06-27 17:45:57 +0300

    mutex fix

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -46,6 +46,7 @@ void MainWindow::playButton_clicked(){
         thread->start();
         firstExec = false;
     }
+    mtx.try_lock();
     mtx.unlock();
 }
 
@@ -59,6 +60,7 @@ void MainWindow::stopButton_clicked(){
 
     emit finished();
     firstExec = true;
+    mtx.try_lock();
     mtx.unlock();
 
     scene->clear();
